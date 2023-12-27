6aa53617e82a6294559dbf74520f4766d435bbbc
yorulmaz1061 <ozan.yorulmaz@gmail.com>
2023-12-27 22:50:49 +0300

    initial commit

---
+++ src/main/java/com/ozan/MyTicketingProjectOrmApplication.java
@@ -0,0 +1,13 @@
+package com.ozan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTicketingProjectOrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTicketingProjectOrmApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/ozan/service/RoleService.java
@@ -0,0 +1,6 @@
+package com.ozan.service;
+
+import com.cydeo.dto.RoleDTO;
+
+public interface RoleService extends CrudService<RoleDTO, Long> {
+}
